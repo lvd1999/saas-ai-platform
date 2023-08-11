dc59b9e9899cf6b95c1354607a4ecd940af84ed7
eleevoo <voon.dic.lee@ericsson.com>
2023-08-11 08:29:28 +0100

    setup Clerk authentication

--- ai-saas/app/(dashboard)/(routes)/dashboard/page.tsx
+++ ai-saas/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,10 @@
-import { Button } from "@/components/ui/button"
+import { UserButton } from "@clerk/nextjs"
 
 export default function DashboardPage() {
   return (
-    <div>Dashboard Page</div>
+    <div>
+      <p>Dashboard Page</p>
+      <UserButton afterSignOutUrl="/"/>
+      </div>
   )
 }
